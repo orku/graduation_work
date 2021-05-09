75d5e376044c6695dbe6e2a9fffb2d8f3f850bac
Artem Shestakov <ottoorku@gmail.com>
2021-05-09 17:53:04 +0300

    added new docs for new ddc motor

--- matlab/driver.m
+++ matlab/driver.m
@@ -10,24 +10,23 @@ a_t = a*sin(w_*t);
 w_t = a*w_*cos(w_*t);
 q_t = a*w_*w_*sin(w_*t);
 Mn = -J.*q_t + k.*abs(w_t).*w_t + M;
-% Mn = -J.*q_t + k.*w_t + M;
 plot(Mn, 10*w_t)
-% xlim([4 16])
-% ylim([-35 35])
+xlim([4 16])
+ylim([-35 35])
 xlabel("Момент, Нм", "FontSize", 14)
 ylabel("Угловая скорость вращения, об/мин", "FontSize", 14)
 grid on
 hold on
 % построение механической характеристики двигателя
 u = 0.87;
-i = 196;%w as 215
-m_ = 539;% was 632
+i = 196;
+m_ = 539;
 m = 0:0.1:m_* i * u * 0.25;
-n0 = 5900/i; % was 7500
+n0 = 5900/i;
 N_M = n0*(1 - m/(m_ * i * u));
 plot(m/1000, N_M)
 
-% legend("Нагрузочна характеристика", "Механическая характеристика двигателя вместе с редуктором")
+legend("Нагрузочна характеристика", "Механическая характеристика двигателя вместе с редуктором", "FontSize", 11)
 
 % i = 215 https://www.faulhaber.com/fileadmin/Import/Media/EN_2668_CR_DFF.pdf
 % mtotor    https://www.faulhaber.com/fileadmin/Import/Media/EN_32GPT_FMM.pdf
